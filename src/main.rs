5acbc7f0e20781d3bd1cbe645b75715c563c34a0
Anna Filippova <annafilippova@Annas-MacBook-Pro-2.local>
2016-10-22 15:09:13 -0400

    Add initial hyper client

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+extern crate hyper;
+use hyper::Client;
+
 fn main() {
-    println!("Hello, world!");
+    let client = Client::new(); // new method on struct 
+    let res = client.get("http://integer32.com")
+                    .send()
+                    .expect("Request failed"); // <---- different
+
+    assert_eq!(res.status, hyper::Ok); // what you should be getting back, different OK from results::Ok
 }
